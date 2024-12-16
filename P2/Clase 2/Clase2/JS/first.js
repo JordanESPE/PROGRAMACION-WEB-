667453300ae3bed011fd24117dfc08c4dde143ea
JordanESPE <166166268+JordanESPE@users.noreply.github.com>
2024-12-16 11:06:07 -0500

    Clase 2 16_12_24

---
+++ P2/Clase 2/Clase2/JS/first.js
@@ -0,0 +1,23 @@
+var nombre = "Ricardo";
+var apellido = "Vargas";
+var edad;
+edad = 25;
+console.log("Mi nombre es " + nombre + " "+apellido);
+console.log("Mi edad es "+  edad);
+
+
+let x = 10;
+let y = 10;
+let z = x + y;
+console.log(z);
+
+
+// Var declara una variable global
+// let declara una variable local o tambien una variable de bloque
+//const declara una constante que no va a cambiar su valor
+//if marca un bloque de isntrucciones que se ejecuta si una condicion se cumple
+//switch marca un bloque de instricciones que se ejecutan si una condicion se cumple
+//for marca un bloque de instrucciones que se ejecutan varias veces
+//while marca un bloque de instrucciones que se ejecutan varias veces
+//function marca un bloque de instrucciones que se ejecutan  varias veces se declara
+//try, catch, returno
