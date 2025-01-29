dddbbcf550ee02ea478d8d65885707619b0db509
¨Jordan Guaman <jaguaman16@espe.edu.ec>
2025-01-29 11:06:06 -0500

    change

--- P2/CONJUNTA PARCIAL 2/ejemplo_setTimeout.js
+++
@@ -1,6 +0,0 @@
-function mostrarMensaje() {
-    console.log("Este mensaje aparece después de 3 segundos");
-}
-
-// Llamar a la función después de 3000 ms (3 segundos)
-setTimeout(mostrarMensaje, 3000);
